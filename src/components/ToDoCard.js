870f18273caa2acb1facd62a3291c5f140f387ee
reireynoso <reireynoso@gmail.com>
2020-01-30 10:03:33 -0500

    added button and changed hint

--- src/components/ToDoCard.js
+++ src/components/ToDoCard.js
@@ -7,9 +7,10 @@ const ToDoCard = (props) => {
           <div className="header">{/*ToDo TITLE*/}</div>
           {/* The button will require some conditional rendering. 
             If the button is under the Incomplete Container, button should be blue and text should say Complete
-            If the button is under Complete Container, button should be red and text should say Incomplete 
+            If the button is under Complete Container, button should be orange and text should say Incomplete 
             */}
           <button onClick={null} className="ui button blue">Change</button>
+          <button onClick={null} className="ui button red">Delete</button>
         </div>
         
     </div>
