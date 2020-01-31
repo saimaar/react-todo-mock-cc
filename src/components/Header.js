0c2f49d9a98e20315d0070634aaa4876677b030f
reireynoso <reireynoso@gmail.com>
2020-01-31 10:12:46 -0500

    changes to Header className and Css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Header = () => {
     return(
-    <div>
-        <div className="ui inverted vertical masthead center aligned segment">
+    <div className="header">
+        <div className="ui inverted vertical masthead center aligned segment header">
             <h1>React ToDo List</h1>
             <div className="image">
-                <img alt="React Logo" src="/logo192.png"/>
+                <img id="react-image" alt="React Logo" src="/logo192.png"/>
             </div>
         </div>
     </div>
